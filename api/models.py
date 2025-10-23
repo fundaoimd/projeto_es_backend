33c9d3ea1ec21824c5546925c0eac9797c495853
hildemberg986 <hildembergeling@gmail.com>
2025-10-23 14:42:25 -0300

    criando CRUD services

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Client(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
@@ -8,11 +9,21 @@ class Client(models.Model):
     def __str__(self):
         return self.name
 
+
 class Employee(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True)
-    role = models.CharField(max_length=100, blank=True) 
+    role = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return f"{self.name} ({self.role})"
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=1000, blank=True)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}"
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ClientViewSet, EmployeeViewSet # Importar o novo ViewSet
+# Importar o novo ViewSet
+from .views import ClientViewSet, EmployeeViewSet, ServicesViewSet
 
 router = DefaultRouter()
 router.register(r'clients', ClientViewSet)
-router.register(r'employees', EmployeeViewSet) 
-
+router.register(r'employees', EmployeeViewSet)
+router.register(r'services', ServicesViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
